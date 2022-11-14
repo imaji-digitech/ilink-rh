861b1f2bbb4314c542ed56a36e00316232d7dbff
masif088 <mokhamadasif@gmail.com>
2022-11-14 15:39:44 +0700

    tabel kegiatan di hub

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -39,55 +39,57 @@
                             <i class="fas fa-home"></i><span> Report bulanan</span>
                         </a>
                     </li>
-{{--                    <li class="sidebar-list">--}}
-{{--                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('driver.index') }}">--}}
-{{--                            <i class="fas fa-car"></i><span> Driver</span>--}}
-{{--                        </a>--}}
-{{--                    </li>--}}
-{{--                    <li class="sidebar-list">--}}
-{{--                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('driver.index') }}">--}}
-{{--                            <i class="fas fa-user"></i><span> User</span>--}}
-{{--                        </a>--}}
-{{--                    </li>--}}
+                    {{--                    <li class="sidebar-list">--}}
+                    {{--                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('driver.index') }}">--}}
+                    {{--                            <i class="fas fa-car"></i><span> Driver</span>--}}
+                    {{--                        </a>--}}
+                    {{--                    </li>--}}
+                    {{--                    <li class="sidebar-list">--}}
+                    {{--                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('driver.index') }}">--}}
+                    {{--                            <i class="fas fa-user"></i><span> User</span>--}}
+                    {{--                        </a>--}}
+                    {{--                    </li>--}}
 
-                    <li class="sidebar-main-title">
-                        <div><h6>Pengolahan</h6></div>
-                    </li>
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('material.index') }}">
-                            <i class="fas fa-recycle"></i><span> Material</span>
-                        </a>
-                    </li>
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('material-mutation') }}">
-                            <i class="fas fa-recycle"></i><span> Mutasi material</span>
-                        </a>
-                    </li>
-                    <li class="sidebar-main-title">
-                        <div><h6>Surat & keuangan</h6></div>
-                    </li>
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('invoice.index') }}">
-                            <i class="fas fa-inbox"></i><span> Invoice</span>
-                        </a>
-                    </li>
+                    @if(config('app.name', 'Laravel')=='Laravel')
+                        <li class="sidebar-main-title">
+                            <div><h6>Pengolahan</h6></div>
+                        </li>
+                        <li class="sidebar-list">
+                            <a class="sidebar-link sidebar-title link-nav" href="{{ route('material.index') }}">
+                                <i class="fas fa-recycle"></i><span> Material</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-list">
+                            <a class="sidebar-link sidebar-title link-nav" href="{{ route('material-mutation') }}">
+                                <i class="fas fa-recycle"></i><span> Mutasi material</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-main-title">
+                            <div><h6>Surat & keuangan</h6></div>
+                        </li>
+                        <li class="sidebar-list">
+                            <a class="sidebar-link sidebar-title link-nav" href="{{ route('invoice.index') }}">
+                                <i class="fas fa-inbox"></i><span> Invoice</span>
+                            </a>
+                        </li>
 
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('receipt.index') }}">
-                            <i class="fas fa-file-archive"></i><span> Kwitansi</span>
-                        </a>
-                    </li>
+                        <li class="sidebar-list">
+                            <a class="sidebar-link sidebar-title link-nav" href="{{ route('receipt.index') }}">
+                                <i class="fas fa-file-archive"></i><span> Kwitansi</span>
+                            </a>
+                        </li>
 
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('travel-permit.index') }}">
-                            <i class="fas fa-truck"></i><span> Surat jalan</span>
-                        </a>
-                    </li>
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title link-nav" href="{{ route('good-receipt.index') }}">
-                            <i class="fas fa-box"></i><span> Terima barang</span>
-                        </a>
-                    </li>
+                        <li class="sidebar-list">
+                            <a class="sidebar-link sidebar-title link-nav" href="{{ route('travel-permit.index') }}">
+                                <i class="fas fa-truck"></i><span> Surat jalan</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-list">
+                            <a class="sidebar-link sidebar-title link-nav" href="{{ route('good-receipt.index') }}">
+                                <i class="fas fa-box"></i><span> Terima barang</span>
+                            </a>
+                        </li>
+                    @endif
 
                     <li class="sidebar-list">
                         <a class="sidebar-link sidebar-title link-nav">
